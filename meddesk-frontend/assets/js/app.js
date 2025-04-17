a68259b2819a33e0602c469fbb5ed3cc39aff165
JakeM02 <jake76@live.ie>
2025-04-17 21:30:43 +0100

    e

--- meddesk-frontend/assets/js/app.js
+++ meddesk-frontend/assets/js/app.js
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Reset footer before adding buttons
         modalFooter.innerHTML = '';
 
-        footer.appendChild(Object.assign(document.createElement('button'), {
+        modalFooter.appendChild(Object.assign(document.createElement('button'), {
             className: 'btn btn-secondary',
             textContent: 'Close',
             type: 'button',
